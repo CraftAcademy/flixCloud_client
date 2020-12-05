4d29903f605c375cb727f413c3f50e74d067e605
William <willspecker@gmail.com>
2020-12-05 11:36:53 +0100

    Creates test to search for list of movies by title creates successful sign up fixture creates search results fixture

---
+++ cypress/integration/userCanSearchForListOfMovies.feature.js
@@ -0,0 +1,35 @@
+describe('User can search for movies', () => {
+  describe('when logged in as registered user', () => {
+    beforeEach(() => {
+      cy.server()
+      cy.route({
+        method: 'POST',
+        url: 'http://localhost:3000/api/auth',
+        response: "fixture:successful_sign_up.json",
+        headers: {
+          uid: 'user@mail.com',
+          access_token: 'token',
+          client: '12345',
+          token_type: "Bearer",
+          expiry: 1000000
+        }
+      })
+      cy.route({
+        url: "http://localhost:3000/api/movies",
+        method: 'GET',
+        response: "fixture:search_for_christmas.json"
+      })
+      cy.visit('/')
+      cy.get('[data-cy="search-input"]').type('christmas')
+      cy.get('[data-cy="search-button"]').click()
+    })
+
+    it('is expected to display search results', () => {
+      cy.get('[data-cy="search-results"]').within(() => {
+        cy.get('li').contains('The Christmas Chronicles: Part Two').should('exist')
+        cy.get('li').contains('Operation Christmas Drop').should('exist')
+        cy.get('li').contains('Angela\'s Christmas Wish').should('exist')
+      })
+    })
+  })
+})
