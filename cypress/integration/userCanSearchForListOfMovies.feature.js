8825c87a3fb7a4a8f1140aced9869a5af77d8be7
Rithreaksa <wonderfulreaksa@gmail.com>
2020-12-05 23:23:31 +0100

    Modify tests to make them passed

--- cypress/integration/userCanSearchForListOfMovies.feature.js
+++ cypress/integration/userCanSearchForListOfMovies.feature.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 describe('User can search for movies', () => {
   describe('when logged in as registered user', () => {
     beforeEach(() => {
@@ -19,7 +20,22 @@ describe('User can search for movies', () => {
         method: 'GET',
         response: "fixture:search_for_christmas_results.json"
       })
+      cy.route({
+        url: "http://localhost:3000/api/movies",
+        method: 'GET',
+        response: "fixture:movies_data.json"
+      })
       cy.visit('/')
+      
+      cy.get("[data-cy='register-button']").click();
+      cy.get("[data-cy='register-form']").within(() => {
+        cy.get("[data-cy='nickname']").type("fbot")
+        cy.get("[data-cy='email']").type("user@mail.com")
+        cy.get("[data-cy='password']").type("password")
+        cy.get("[data-cy='password-confirmation']").type("password")
+      });
+      cy.get("[data-cy='signup-button']").click();
+
       cy.get('[data-cy="search-input"]').type('christmas')
       cy.get('[data-cy="search-button"]').click()
     })
@@ -31,6 +47,7 @@ describe('User can search for movies', () => {
         cy.contains('Angela\'s Christmas Wish').should('exist')
       })
     })
+
     it('is expected to not display the movie index', () => {
       cy.get('[data-cy="index"]').should("not.exist")
     });
--- cypress/integration/userCanSignUpAndRegister.feature.js
+++ cypress/integration/userCanSignUpAndRegister.feature.js
@@ -3,6 +3,18 @@ describe('User authenticates', () => {
   beforeEach(() => {
     cy.server();
     cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/api/auth',
+      response: "fixture:successful_sign_up.json",
+      headers: {
+        uid: 'user@mail.com',
+        access_token: 'token',
+        client: '12345',
+        token_type: "Bearer",
+        expiry: 1000000
+      }
+    });
+    cy.route({
       method: "GET",
       url: "http://localhost:3000/api/movies",
       response: "fixture:movies_data.json",
