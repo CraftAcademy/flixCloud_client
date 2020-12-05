47dc8613a63f93efb8a41c23ba220002ef0b1a2b
William <willspecker@gmail.com>
2020-12-05 12:23:49 +0100

    creates search functionality maps over movies to yield search results edits fixture to match key used in search component

--- src/components/SearchForMovies.jsx
+++ src/components/SearchForMovies.jsx
@@ -1,36 +1,43 @@
-import React, { Component } from 'react'
-import { Button, Input } from 'semantic-ui-react'
-import axios from 'axios'
+import React, { Component } from "react";
+import { Button, Input } from "semantic-ui-react";
+import axios from "axios";
 
 class SearchForMovies extends Component {
   state = {
-    searchValue: '',
-    movieSearchResults: []
+    searchValue: "",
+    movieSearchResults: [],
+  };
+
+  setSearchInputValue(e) {
+    this.setState({ searchValue: e.target.value });
   }
 
   async searchByTitle() {
-    const query = this.state.searchValue
-    let response = await axios.get('https://localhost:3000/api/movies')
-    this.setState({ movieSearchResults: response.data.movies })
+    // const query = this.state.searchValue
+    let response = await axios.get("/movies");
+    this.setState({ movieSearchResults: response.data.movies });
   }
 
   render() {
+    let displaySearchResults = this.state.movieSearchResults.map((movie) => {
+      return <li movie={movie} key={movie.id}>{movie.title}</li>;
+    });
     return (
       <>
-      <Input 
-        type='text'
-        data-cy='search-input'
-        placeholder='Search By Title...'
-      
-      />
-        <Button 
-          onClick={()=> this.searchByTitle()}
-          data-cy='search-button'>
+        <Input
+          type="text"
+          data-cy="search-input"
+          placeholder="Search By Title..."
+        />
+        <Button onClick={() => this.searchByTitle()} data-cy="search-button">
           Search
         </Button>
+        <div data-cy="search-results">
+          <ul>{displaySearchResults}</ul>
+        </div>
       </>
-    )
+    );
   }
 }
 
-export default SearchForMovies
+export default SearchForMovies;
