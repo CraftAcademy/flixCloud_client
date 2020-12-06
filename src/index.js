b6c734fd7abba0dc09b3b9e97956a6a2779b681d
Ehugo2000 <erik.hgsn@gmail.com>
2020-12-06 17:02:01 +0100

    Continue to add testing for Stripe Installing Stripe Adding apiKEY from Stripe

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
-import "semantic-ui-css/semantic.min.css"
+import "semantic-ui-css/semantic.min.css";
+import { StripeProvider } from "react-stripe";
 
 let apiUrl;
 apiUrl = "http://localhost:3000/api";
@@ -11,9 +12,10 @@ apiUrl = "http://localhost:3000/api";
 axios.defaults.baseURL = apiUrl;
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StripeProvider apiKey="pk_test_51HuxhoEDdj3L9cb7mRwXVf3mjnVY7dk7NL7WmIZ31HGjkKHk9RSQASdCWIXiObHTiyfYkNhxKFsvLFkvLKyxM6Wz004BVP7pWj">
     <App />
-  </React.StrictMode>,
+  </StripeProvider>,
+
   document.getElementById("root")
 );
 
