e6b5c759e471f4b16b9fb1498bcdb3b8ffd19066
Rithreaksa <wonderfulreaksa@gmail.com>
2020-12-06 16:24:07 +0100

    Add code in subscribe modal

--- src/components/SubscribeModal.jsx
+++ src/components/SubscribeModal.jsx
@@ -4,16 +4,16 @@ import { Button, Modal, Form } from "semantic-ui-react";
 function SubscribeModal({onAuthenticate}) {
   const [open, setOpen] = React.useState(false);
 
-//   const onCancelHandler = (event) => {
-//     event.preventDefault();
-//     setOpen(false)
-//   }
+  const onCancelHandler = (event) => {
+    event.preventDefault();
+    setOpen(false)
+  }
 
-//   const onAuthenticateHandler = async (event) => {
-//     event.preventDefault();
-//     const successful = await onAuthenticate(event)
-//     setOpen(!successful)
-//   }
+  const onAuthenticateHandler = async (event) => {
+    event.preventDefault();
+    const successful = await onAuthenticate(event)
+    setOpen(!successful)
+  }
 
   return (
     <Modal
@@ -23,54 +23,43 @@ function SubscribeModal({onAuthenticate}) {
       trigger={
         <Button
           data-cy="subscribe-button"
-          onClick={() => setOpen({ renderSubscribeForm: true })}
+          onClick={() => setOpen({ renderRegisterForm: true })}
         >
           Subscribe
         </Button>
       }
-      ></Modal>
-    // >
-//       <Modal.Header>Fill in form to register</Modal.Header>
-//       <Form data-cy="register-form" onSubmit={onAuthenticateHandler}>
-//         <Form.Field>
-//           <label>Choose a nickname</label>
-//           <input name="nickname" data-cy="nickname" type="text" placeholder="Nickname" />
-//         </Form.Field>
+      >
+  
+      <Modal.Header>Fill in your card information</Modal.Header>
+      <Form data-cy="payment-form" onSubmit={onAuthenticateHandler}>
+        <Form.Field>
+          <label>Card Number</label>
+          <input name="card-number" data-cy="card-number" type="number" placeholder="Card Number" />
+        </Form.Field>
+        <Form.Field>
+          <label>Expiry Date</label>
+          <input name="expiry-date" data-cy="expiry-date" type="number" placeholder="Expiry Date" />
+        </Form.Field>
 
-//         <Form.Field>
-//           <label className="email">Email</label>
-//           <input name="email" data-cy="email" type="text" placeholder="Email" />
-//         </Form.Field>
+        <Form.Field>
+          <label>CVC</label>
+          <input name="cvc" data-cy="cvc" type="number" placeholder="CVC" />
+        </Form.Field>
 
-//         <Form.Field>
-//           <label className="password">Password</label>
-//           <input name="password" data-cy="password" type="password" placeholder="password" />
-//         </Form.Field>
-
-//         <Form.Field>
-//           <label className="password-confirmation">Repeat password</label>
-//           <input
-//             name="password_confirmation"
-//             data-cy="password-confirmation"
-//             type="password"
-//             placeholder="Repeat your password"
-//           />
-//         </Form.Field>
-
-//         <Modal.Actions>
-//           <Button color="black" onClick={onCancelHandler}>
-//             Nope
-//           </Button>
-//           <Button
-//             data-cy="signup-button"
-//             content="Yes please!"
-//             labelPosition="right"
-//             icon="checkmark"
-//             positive
-//           />
-//         </Modal.Actions>
-//       </Form>
-//     </Modal>
+        <Modal.Actions>
+          <Button color="black" onClick={onCancelHandler}>
+            Nope
+          </Button>
+          <Button
+            data-cy="signup-button"
+            content="Yes please!"
+            labelPosition="right"
+            icon="checkmark"
+            positive
+          />
+        </Modal.Actions>
+      </Form>
+    </Modal>
   );
 }
 
