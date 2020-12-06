f5d507ec9e0313071d42dc17db0e53531facd647
Rithreaksa <wonderfulreaksa@gmail.com>
2020-12-06 23:38:28 +0100

    Fix baseUrl

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "semantic-ui-css/semantic.min.css";
 import { StripeProvider } from "react-stripe-elements";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV !== "production"
+  process.env.NODE_ENV === "production"
     ? "https://flixcloud-api.herokuapp.com/api"
     : "http://localhost:3000/api";
 
